db0722e707aac43e900c2c617b49ec2c06b1af85
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-28 11:52:14 +0300

    set up

---
+++ app/Http/Controllers/BotController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Inspiring;
+use Telegram\Bot\Api;
+
+/**
+ * Class BotController
+ */
+class BotController extends Controller
+{
+    /** @var Api */
+    protected $telegram;
+
+    /**
+     * BotController constructor.
+     *
+     * @param Api $telegram
+     */
+    public function __construct(Api $telegram)
+    {
+        $this->telegram = $telegram;
+    }
+
+    /**
+     * Get updates from Telegram.
+     */
+    public function getUpdates()
+    {
+        $updates = $this->telegram->getUpdates();
+    }
+
+    /**
+     * Set a webhook.
+     */
+    public function setWebhook()
+    {
+        $url = "https://fe4bd30e.ngrok.io";
+        return $this->telegram->setWebhook(['url' => $url]);
+    }
+
+    /**
+     * Remove webhook.
+     *
+     * @return array
+     */
+    public function removeWebhook()
+    {
+        $response = $this->telegram->removeWebhook();
+
+        return $response->getDecodedBody();
+    }
+
+    /**
+     * Handles incoming webhook updates from Telegram.
+     *
+     * @return string
+     */
+    public function webhookHandler()
+    {
+        return $this->telegram->commandsHandler(true);
+    }
+}
